6e23207a41df2c462ae160a71748f8177965e990
laveezahzargar <laveezahzargar@gmail.com>
2026-06-19 13:11:44 +0530

    fixes22

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:5238/api"
+    baseURL: "http://localhost:5238/api,https://storybloom-backend-zm26.onrender.com"
 });
 
 api.interceptors.request.use(config => {
